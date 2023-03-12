b63313daa03dcb492f6f05191738ba591f98922b
Dmitriy <tiemposilencioo@gmail.com>
2023-03-12 13:01:13 +0300

    caseRender

--- src/components/palette/Pallete.tsx
+++ src/components/palette/Pallete.tsx
@@ -4,49 +4,63 @@ import './palette.scss';
 const Pallete = (): JSX.Element => {
 
     const [boards, setBoards] = useState([
-        { id: 1, items: [{}, {}, {}, {}] },
-        { id: 2, items: [{}, {}, {}, {}] }
+        {
+            id: 1, items: [
+                { id: 1, type: "input" },
+                { id: 2, operations: [{ titleOperations: "/" }, { titleOperations: "x" }, { titleOperations: "-" }, { titleOperations: "+" }], type: "operations" },
+                { id: 3, type: "dial", numbers: [{ titleNumbers: "7" }, { titleNumbers: "8" }, { titleNumbers: "9" }, { titleNumbers: "4" }, { titleNumbers: "5" }, { titleNumbers: "6" }, { titleNumbers: "1" }, { titleNumbers: "2" }, { titleNumbers: "3" }, { titleNumbers: "0" }, { titleNumbers: "," }] },
+                { id: 4, type: "equally", titleEqually: "=" }]
+        },
+
     ])
 
-    return (
-        <>
-            {
-                boards.map(() =>
-                    <section className='pallete'>
-                        <div className="pallete__display">
-                            <input type="text" className="pallete__display-input" />
+    const elements: React.ReactNode = boards.map((item) => {
+        return item.items.map(item => {
+            switch (item.type) {
+                case 'input':
+                    return <div key={item.id} className="pallete__display">
+                        <input placeholder="0" type="text" className="pallete__display-input" />
+                    </div>;
+                case 'operations':
+                    return <div key={item.id} className="pallete__operations">
+                        <div className="pallete__operations-wrapp">
+                            {item.operations?.map((item =>
+                                <button
+                                    key={item.titleOperations}
+                                    className="pallete__operations-buttons">{item.titleOperations}</button>
+                            ))}
                         </div>
-                        <div className="pallete__operations">
-                            <div className="pallete__operations-wrapp">
-                                <button className="pallete__operations-buttons">/</button>
-                                <button className="pallete__operations-buttons">x</button>
-                                <button className="pallete__operations-buttons">-</button>
-                                <button className="pallete__operations-buttons">+</button>
-                            </div>
+                    </div>
+                case 'dial':
+                    return <div
+                        key={item.id}
+                        className="pallete__dial">
+                        <div className="pallete__dial-wrapp">
+                            {item.numbers?.map(item =>
+                                <button
+                                    key={item.titleNumbers}
+                                    className="pallete__dial-button">{item.titleNumbers}</button>
+                            )}
                         </div>
-                        <div className="pallete__dial">
-                            <div className="pallete__dial-wrapp">
-                                <button className="pallete__dial-button">9</button>
-                                <button className="pallete__dial-button">8</button>
-                                <button className="pallete__dial-button">7</button>
-                                <button className="pallete__dial-button">6</button>
-                                <button className="pallete__dial-button">5</button>
-                                <button className="pallete__dial-button">4</button>
-                                <button className="pallete__dial-button">3</button>
-                                <button className="pallete__dial-button">2</button>
-                                <button className="pallete__dial-button">1</button>
-                                <button className="pallete__dial-button">0</button>
-                                <button className="pallete__dial-button">,</button>
-                            </div>
-                        </div>
-                        <div className="pallete__equally">
-                            <div className="pallete__equally-wrapp">=</div>
-                        </div>
-                    </section>
-                )
+                    </div>
+                case 'equally':
+                    return <div key={item.id} className="pallete__equally">
+                        <div className="pallete__equally-wrapp">{item.titleEqually}</div>
+                    </div>
+                default:
+                    return null;
             }
-        </>
+        }
+        )
+    });
+
+
+    return (
+        <section className="pallet">
+            {elements}
+        </section>
     )
+
 }
 
 export default Pallete
